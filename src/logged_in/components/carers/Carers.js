95a808d71b0dad6537475c7bad98bd8029bd766a
seetvn <waiyansteven2002@gmail.com>
2024-03-05 02:39:24 +0000

    added route to button + collect and read data from newcarers

--- src/logged_in/components/carers/Carers.js
+++ src/logged_in/components/carers/Carers.js
@@ -3,20 +3,30 @@ import PropTypes from "prop-types";
 import BasicTable from "./CarTable";
 import Button from "@mui/material/Button"; // Import Button component from Material-UI
 import Grid from "@mui/material/Grid"; // Import Grid component from Material-UI
+import { useHistory } from 'react-router-dom'; 
+import { useLocation } from 'react-router-dom';
 
 
 function Carers(props) {
     const {
         selectCarers,
       } = props;
+      const history = useHistory();
+      const location = useLocation();
+      const { formData } = location.state;
     
       useEffect(selectCarers, [selectCarers]);
 
       const handleAddNewCarer = () => {
         // Add logic to handle adding new carer here
         console.log("Add New Carer button clicked");
+        history.push("/c/new-carer-page"); // Specify the path of the new page
     };
+    // --debug function---
+  //   const test_func = () => {
+  //     console.log(formData);
     
+  // };
 
     return (
         <Fragment>
@@ -26,7 +36,7 @@ function Carers(props) {
               </Button>
             </Grid>
           <br /> {/* Add line break for spacing */}
-          <BasicTable/>
+          <BasicTable carersData={[formData]}/>
         </Fragment>
       );
     }
